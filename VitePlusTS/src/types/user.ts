6d62e3847eb3f5fae69d67bb48ba831c38479bea
TimiPashov <pashov91@gmail.com>
2024-04-23 15:06:51 +0300

    configure data types

---
+++ VitePlusTS/src/types/user.ts
@@ -0,0 +1,12 @@
+import { Address, Company } from "./common";
+
+export type User = {
+  id: 1;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+};
